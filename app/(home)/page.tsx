9fa024ab395056bba355934f8d32ed9ec4afa429
lgsandy <lgsandy543@gmail.com>
2025-02-23 22:47:59 +0530

    animation is implemented

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,20 @@
+import { Header } from "@/components/home/header";
+import { HeroSection } from "@/components/home/hero-section";
 import LoginWithgoogl from "@/components/login-with-google";
 import { AuthContextProvider } from "@/lib/context/AuthContext";
+import {ClientSection} from "@/components/home/client-section";
+import {FeatureSection} from "@/components/home/feature-section";
+import {FooterSection} from "@/components/home/footer-section";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <AuthContextProvider><LoginWithgoogl /></AuthContextProvider>
-    </div>
+    <>
+      {/* <AuthContextProvider><LoginWithgoogl /></AuthContextProvider> */}
+      <Header />
+      <HeroSection />
+      <ClientSection />
+      <FeatureSection />
+        <FooterSection />
+    </>
   );
 }
