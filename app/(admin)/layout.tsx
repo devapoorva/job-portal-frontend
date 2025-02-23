9e340145435126837e90019f180563a133fa8c7e
lgsandy <lgsandy543@gmail.com>
2025-02-23 20:27:40 +0530

    light dark theme is implemented

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -16,6 +16,8 @@ import {
 } from "@/components/ui/sidebar";
 import "../globals.css";
 import { AuthContextProvider} from "@/lib/context/AuthContext";
+import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeSwitcher } from "@/components/theme-switcher";
 
 
 
@@ -29,11 +31,18 @@ export default function AdminLayout({
     <html lang="en">
       <body>
         <AuthContextProvider>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          > 
         <SidebarProvider>
           <AppSidebar />
           <SidebarInset>
             <header className="flex sticky top-0  h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 border-b">
-              <div className="flex items-center gap-2 px-4">
+              <div className="flex items-center justify-between gap-2 px-4 w-full">
+                <div className="flex items-cente">
                 <SidebarTrigger className="-ml-1" />
                 <Separator orientation="vertical" className="mr-2 h-4" />
                 <Breadcrumb>
@@ -49,11 +58,16 @@ export default function AdminLayout({
                     </BreadcrumbItem>
                   </BreadcrumbList>
                 </Breadcrumb>
+                </div>
+                <div>
+                  <ThemeSwitcher />
+                </div>
               </div>
             </header>
             <div className="flex flex-1  p-4 pt-0">{children}</div>
           </SidebarInset>
         </SidebarProvider>
+        </ThemeProvider> 
         </AuthContextProvider>
       </body>
     </html>
