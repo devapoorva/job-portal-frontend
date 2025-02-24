8eb67aa764972c04e5a39491178c0e734050c31d
lgsandy <lgsandy543@gmail.com>
2025-02-24 23:18:10 +0530

    Authencation is implemented

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -15,9 +15,11 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import "../globals.css";
-import { AuthContextProvider} from "@/lib/context/AuthContext";
+import {AuthContextProvider, useAuth} from "@/lib/context/AuthContext";
 import { ThemeProvider } from "@/components/theme-provider"
 import { ThemeSwitcher } from "@/components/theme-switcher";
+import {useEffect} from "react";
+import {useRouter} from "next/navigation";
 
 
 
@@ -26,50 +28,44 @@ export default function AdminLayout({
 }: {
   children: React.ReactNode;
 }) {
-
-  return (
-    <html lang="en">
-      <body>
-        <AuthContextProvider>
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          > 
-        <SidebarProvider>
-          <AppSidebar />
-          <SidebarInset>
-            <header className="flex sticky top-0  h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 border-b">
-              <div className="flex items-center justify-between gap-2 px-4 w-full">
-                <div className="flex items-cente">
-                <SidebarTrigger className="-ml-1" />
-                <Separator orientation="vertical" className="mr-2 h-4" />
-                <Breadcrumb>
-                  <BreadcrumbList>
-                    <BreadcrumbItem className="hidden md:block">
-                      <BreadcrumbLink href="#">
-                        Building Your Application
-                      </BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator className="hidden md:block" />
-                    <BreadcrumbItem>
-                      <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                    </BreadcrumbItem>
-                  </BreadcrumbList>
-                </Breadcrumb>
+  const router = useRouter();
+  const {user,loading } = useAuth()
+  useEffect(() => {
+    if (!loading && !user) {
+      router.push("/login");
+    }
+  }, [user, loading, router]);
+  if (loading) return <p>Loading...</p>;
+  return user ? (
+          <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+              <header className="flex sticky top-0  h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 border-b">
+                <div className="flex items-center justify-between gap-2 px-4 w-full">
+                  <div className="flex items-cente">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                    <Breadcrumb>
+                      <BreadcrumbList>
+                        <BreadcrumbItem className="hidden md:block">
+                          <BreadcrumbLink href="#">
+                            Building Your Application
+                          </BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator className="hidden md:block" />
+                        <BreadcrumbItem>
+                          <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                        </BreadcrumbItem>
+                      </BreadcrumbList>
+                    </Breadcrumb>
+                  </div>
+                  <div>
+                    <ThemeSwitcher />
+                  </div>
                 </div>
-                <div>
-                  <ThemeSwitcher />
-                </div>
-              </div>
-            </header>
-            <div className="flex flex-1  p-4 pt-0">{children}</div>
-          </SidebarInset>
-        </SidebarProvider>
-        </ThemeProvider> 
-        </AuthContextProvider>
-      </body>
-    </html>
-  );
+              </header>
+              <div className="flex flex-1  p-4 pt-0">{children}</div>
+            </SidebarInset>
+          </SidebarProvider>
+  ) : null;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { Header } from "@/components/home/header";
 import { HeroSection } from "@/components/home/hero-section";
-import LoginWithgoogl from "@/components/login-with-google";
-import { AuthContextProvider } from "@/lib/context/AuthContext";
 import {ClientSection} from "@/components/home/client-section";
 import {FeatureSection} from "@/components/home/feature-section";
 import {FooterSection} from "@/components/home/footer-section";
@@ -9,7 +7,6 @@ import {FooterSection} from "@/components/home/footer-section";
 export default function Home() {
   return (
     <>
-      {/* <AuthContextProvider><LoginWithgoogl /></AuthContextProvider> */}
       <Header />
       <HeroSection />
       <ClientSection />
