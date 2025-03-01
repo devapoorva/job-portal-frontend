c84937fb489fd541fbc9a030cfb324f93d0db989
lgsandy <lgsandy543@gmail.com>
2025-03-01 13:10:48 +0530

    login api integration is ongoing

--- app/services/register-service.ts
+++ app/services/register-service.ts
@@ -5,9 +5,9 @@ import { RegisterUser } from '@/lib/types/register';
 
 export const registerService = {
   // Register
-  createUser: async (userData: RegisterUser) => {
-    const response = await http.post<User>('/users', userData);
-    return response.data;
-  },
+//   registerUser: async (userData: RegisterUser) => {
+//     const response = await http.post<User>('/users', userData);
+//     return response.data;
+//   },
 
 };
