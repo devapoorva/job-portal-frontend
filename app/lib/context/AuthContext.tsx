7b347088574164bcce08ca7490a0c771ca50467b
lgsandy <lgsandy543@gmail.com>
2025-03-02 19:32:28 +0530

    Register issues is fixed & role check is implemented

--- app/lib/context/AuthContext.tsx
+++ app/lib/context/AuthContext.tsx
@@ -32,7 +32,6 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      console.log('currentUser',currentUser)
       setUser(currentUser);
       setLoading(false);
     });
